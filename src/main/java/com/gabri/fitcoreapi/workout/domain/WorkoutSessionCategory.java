e8bb8d576f50e1913eafdd9ba964680522dbd584
Gabriel <93080714+Gabri-2001@users.noreply.github.com>
2026-04-20 19:22:15 +0200

    feat: add workout session enums

---
+++ src/main/java/com/gabri/fitcoreapi/workout/domain/WorkoutSessionCategory.java
@@ -0,0 +1,10 @@
+package com.gabri.fitcoreapi.workout.domain;
+
+public enum WorkoutSessionCategory {
+    STRENGTH,
+    HYPERTROPHY,
+    ENDURANCE,
+    CARDIO,
+    MOBILITY,
+    MIXED
+}
---
+++ src/main/java/com/gabri/fitcoreapi/workout/domain/WorkoutSessionIntensity.java
@@ -0,0 +1,8 @@
+package com.gabri.fitcoreapi.workout.domain;
+
+public enum WorkoutSessionIntensity {
+    LOW,
+    MODERATE,
+    HIGH,
+    VERY_HIGH
+}
