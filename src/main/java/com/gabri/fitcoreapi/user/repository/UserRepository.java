b6ca2dfdc166f940cc32081c82f5aadccae446d4
Gabriel <93080714+Gabri-2001@users.noreply.github.com>
2026-04-22 12:41:36 +0200

    feat: add user repository

---
+++ src/main/java/com/gabri/fitcoreapi/user/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.gabri.fitcoreapi.user.repository;
+
+import com.gabri.fitcoreapi.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
