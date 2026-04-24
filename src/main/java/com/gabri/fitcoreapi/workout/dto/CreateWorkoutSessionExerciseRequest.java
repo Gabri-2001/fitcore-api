6e4a3a20962850eae549f7c0eca1f3d826fad521
Gabriel <93080714+Gabri-2001@users.noreply.github.com>
2026-04-24 21:22:34 +0200

    refactor: add proper validation key for workout exercise id requirement

--- src/main/java/com/gabri/fitcoreapi/workout/dto/CreateWorkoutSessionExerciseRequest.java
+++ src/main/java/com/gabri/fitcoreapi/workout/dto/CreateWorkoutSessionExerciseRequest.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 public class CreateWorkoutSessionExerciseRequest {
 
-    @NotNull(message = "{error.workoutExercise.notFound}")
+    @NotNull(message = "{validation.workoutSessionExercise.workoutExerciseId.required}")
     private Long workoutExerciseId;
 
     private boolean completed;
